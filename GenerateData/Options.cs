e80763d5ea822de2289d32fe1ccda46946c7e30d
WarFire5 <tatyanamirvoda@yandex.ru>
2024-06-13 23:04:04 +0300

    Added options into gitignore

--- GenerateData/Options.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GenerateData;
-
-public class Options
-{
-    public const string CrmConnectionString = "server=194.87.210.5;Port=5432;database=crm;username=postgres;password=qwe!23";
-    public const string TsConnectionString = "server=194.87.210.5;Port=5432;database=transaction_store_dev;username=postgres;password=qwe!23";
-}
