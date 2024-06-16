1c1695062bd1468baa2cdc96aed0ea55f863562f
WarFire5 <tatyanamirvoda@yandex.ru>
2024-06-16 18:42:22 +0300

    Added BULK INSERT

--- GenerateData/Program.cs
+++ GenerateData/Program.cs
@@ -1,4 +1,5 @@
-﻿using GenerateData.Enums;
+﻿using EFCore.BulkExtensions;
+using GenerateData.Enums;
 using Microsoft.EntityFrameworkCore;
 
 namespace GenerateData;
@@ -7,113 +8,198 @@ public static class Program
 {
     public static async Task Main()
     {
+        // Подключение к CRM контексту и получение первых 3000236 аккаунтов
         await using var crmContext = new CrmContext();
-        var accounts = await crmContext.Accounts.ToListAsync();
+        var accounts = await crmContext.Accounts.Select(t=>t.Id).ToListAsync();
+        Console.WriteLine("Подключение к CRM контексту и получение первых 3000236 аккаунтов.");
+        
+        
+        // string filePath = "account_ids.txt";
+        // await File.WriteAllLinesAsync(filePath, accounts.Select(id => id.ToString()));
+        // Console.WriteLine($"Account IDs written to {filePath}");
+          // Шаг 1: Установим общее количество нужных транзакций
+   // Шаг 1: Установим общее количество нужных транзакций
+            int totalTransactions = 30002358;
 
-        await using var tSContext = new TransactionStoreContext();
+            // Шаг 2: Рассчитаем количество каждого типа транзакций
+            int totalTransferTransactions = (int)Math.Ceiling(totalTransactions * 0.7); // 70% на Трансферы
+            int totalWithdrawTransactions = (int)Math.Floor(totalTransactions * 0.10); // 10% на Вывод
+            int totalDepositTransactions = totalTransactions - totalTransferTransactions - totalWithdrawTransactions; // Оставшиеся на Ввод
 
-        int count = 0;
-        Random random = new Random(); // Чтобы избежать создания одинаковых значений
-
-        foreach (var account in accounts)
-        {
-            // Добавление депозитных транзакций
-            for (int i = 0; i < 2; i++)
+            // Убедимся, что количество трансферных транзакций четное
+            if (totalTransferTransactions % 2 != 0)
             {
-                DateTime startDateTime = new DateTime(2020, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-                DateTime endDateTime = DateTime.UtcNow;
-                DateTime date = GetRandomDateTime(startDateTime, endDateTime);
-
-                TransactionDto deposit = new TransactionDto()
-                {
-                    AccountId = account.Id,
-                    Amount = GetRandomWholeAmount(random),
-                    TransactionType = TransactionType.Deposit,
-                    Date = date
-                };
-                await tSContext.Transactions.AddAsync(deposit);
+                totalTransferTransactions--; // Уменьшаем на 1, чтобы сделать четным
+                totalDepositTransactions++; // Компенсируем добавлением 1 к Вводам
             }
 
-            // Добавление withdrawal транзакций
-            for (int i = 0; i < 1; i++)
-            {
-                DateTime startDateTime = new DateTime(2020, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-                DateTime endDateTime = DateTime.UtcNow;
-                DateTime date = GetRandomDateTime(startDateTime, endDateTime);
+            // // Шаг 3: Считываем идентификаторы аккаунтов из файла
+            // var readAccountIds = await ReadAccountIdsFromFile("account_ids.txt");
 
-                TransactionDto withdraw = new TransactionDto()
-                {
-                    AccountId = account.Id,
-                    Amount = GetRandomWholeAmount(random) * -1,
-                    TransactionType = TransactionType.Withdraw,
-                    Date = date
-                };
-                await tSContext.Transactions.AddAsync(withdraw);
-            }
+            // Шаг 4: Подключаемся к контексту базы данных
+            await using var tSContext = new TransactionStoreContext();
+            Random random = new Random();
+            Console.WriteLine("Подключение к TransactionStore контексту для добавления транзакций.");
 
-            // Добавление transfer транзакций
-            for (int i = 0; i < 4; i++)
-            {
-                DateTime startDateTime = new DateTime(2020, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-                DateTime endDateTime = DateTime.UtcNow;
-                DateTime date = GetRandomDateTime(startDateTime, endDateTime);
+            // Шаг 5: Добавляем транзакции
+            await AddTransactions(tSContext, accounts, totalWithdrawTransactions, totalDepositTransactions, totalTransferTransactions, random);
 
-                TransactionDto transferWithdraw = new TransactionDto()
-                {
-                    AccountId = account.Id,
-                    Amount = GetRandomWholeAmount(random) * -1,
-                    TransactionType = TransactionType.Transfer,
-                    Date = date
-                };
-
-                TransactionDto transferDeposit = new TransactionDto()
-                {
-                    AccountId = account.Id,
-                    Amount = GetRandomNonZeroFractionalAmount(random),
-                    TransactionType = TransactionType.Transfer,
-                    Date = date
-                };
-
-                await tSContext.Transactions.AddAsync(transferWithdraw);
-                await tSContext.Transactions.AddAsync(transferDeposit);
-            }
-
-            count++;
-
-            if (count >= 3818182) break;
+            Console.WriteLine("Транзакции успешно добавлены.");
         }
 
-        await tSContext.SaveChangesAsync();
-        Console.WriteLine("Transactions added successfully.");
-    }
-
-    public static DateTime GetRandomDateTime(DateTime startDate, DateTime endDate)
-    {
-        Random random = new Random();
-        int range = (endDate - startDate).Days;
-        int randomDays = random.Next(range);
-        int randomMilliseconds = random.Next(0, 86400000); // 86400000 миллисекунд = 24 часа
-
-        DateTime randomDate = startDate.AddDays(randomDays).AddMilliseconds(randomMilliseconds);
-
-        return randomDate;
-    }
-
-    public static decimal GetRandomWholeAmount(Random random)
-    {
-        return random.Next(1, 1000000);
-    }
-
-    public static decimal GetRandomNonZeroFractionalAmount(Random random)
-    {
-        int integerPart = random.Next(1, 1000000);
-        double fractionalPart;
-        do
+        public static async Task AddTransactions(TransactionStoreContext context, List<Guid> accountIds, int withdrawCount, int depositCount, int transferCount, Random random)
         {
-            fractionalPart = random.NextDouble(); // Случайное число от 0.0 до 1.0
-        } while (Math.Abs(fractionalPart) < 1e-6); // Повторяем, пока дробная часть не станет ненулевой
+            // Распределение транзакций по типам (Вывод, Ввод, Трансфер)
+            await AddRandomTransactionsWithRetry(context, accountIds, withdrawCount, TransactionType.Withdraw, random);
+            await AddRandomTransactionsWithRetry(context, accountIds, depositCount, TransactionType.Deposit, random);
+            await AddRandomTransferTransactionsWithRetry(context, accountIds, transferCount / 2, random); // Разделим на 2, т.к. каждая трансферная транзакция состоит из двух операций
+        }
 
-        decimal amount = (decimal)(integerPart + fractionalPart);
-        return Math.Round(amount, 4); // Округление до 4 знаков после запятой
-    }
+        public static async Task AddRandomTransactionsWithRetry(TransactionStoreContext context, List<Guid> accountIds, int batchSize, TransactionType type, Random random)
+        {
+            DateTime startDateTime = new DateTime(2020, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            DateTime endDateTime = DateTime.UtcNow;
+
+            int totalTransactionsToAdd = batchSize;
+            int index = 0;
+
+            while (totalTransactionsToAdd > 0)
+            {
+                List<TransactionDto> transactions = new List<TransactionDto>();
+
+                for (int i = 0; i < batchSize && index < accountIds.Count; i++)
+                {
+                    DateTime date = GetRandomDateTime(startDateTime, endDateTime);
+                    Guid accountId = accountIds[index];
+
+                    TransactionDto transaction = new TransactionDto()
+                    {
+                        Id = Guid.NewGuid(), // Генерация уникального идентификатора
+                        AccountId = accountId,
+                        Amount = type == TransactionType.Withdraw
+                            ? -GetRandomNonZeroFractionalAmount(random)
+                            : GetRandomNonZeroFractionalAmount(random),
+                        TransactionType = type,
+                        Date = date
+                    };
+
+                    transactions.Add(transaction);
+                    index++;
+                }
+
+                await BulkInsertWithRetry(context, transactions);
+                totalTransactionsToAdd -= batchSize;
+            }
+        }
+
+        public static async Task AddRandomTransferTransactionsWithRetry(TransactionStoreContext context, List<Guid> accountIds, int batchSize, Random random)
+        {
+            DateTime startDateTime = new DateTime(2020, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            DateTime endDateTime = DateTime.UtcNow;
+
+            int totalTransactionsToAdd = batchSize * 2; // Умножаем на 2, т.к. каждая трансферная транзакция состоит из двух операций
+            int index = 0;
+
+            while (totalTransactionsToAdd > 0)
+            {
+                List<TransactionDto> transactions = new List<TransactionDto>();
+
+                for (int i = 0; i < batchSize && index < accountIds.Count; i++)
+                {
+                    DateTime date = GetRandomDateTime(startDateTime, endDateTime);
+                    Guid accountId = accountIds[index];
+
+                    decimal amount = GetRandomNonZeroFractionalAmount(random);
+
+                    TransactionDto transferWithdraw = new TransactionDto()
+                    {
+                        Id = Guid.NewGuid(), // Генерация уникального идентификатора
+                        AccountId = accountId,
+                        Amount = -amount,
+                        TransactionType = TransactionType.Transfer,
+                        Date = date
+                    };
+
+                    TransactionDto transferDeposit = new TransactionDto()
+                    {
+                        Id = Guid.NewGuid(), // Генерация уникального идентификатора
+                        AccountId = accountId,
+                        Amount = amount,
+                        TransactionType = TransactionType.Transfer,
+                        Date = date
+                    };
+
+                    transactions.Add(transferWithdraw);
+                    transactions.Add(transferDeposit);
+                    index++;
+                }
+
+                await BulkInsertWithRetry(context, transactions);
+                totalTransactionsToAdd -= batchSize * 2;
+            }
+        }
+
+        public static async Task BulkInsertWithRetry(TransactionStoreContext context, List<TransactionDto> transactions)
+        {
+            const int maxRetries = 10;
+            int retryCount = 0;
+            bool success = false;
+
+            while (!success && retryCount < maxRetries)
+            {
+                try
+                {
+                    await context.BulkInsertAsync(transactions);
+                    context.ChangeTracker.Clear(); // Очищение контекста между вставками
+                    success = true;
+                }
+                catch (Exception ex)
+                {
+                    retryCount++;
+                    Console.WriteLine($"Ошибка при пакетной вставке, повтор {retryCount}/{maxRetries}. Ошибка: {ex.Message}");
+
+                    if (retryCount >= maxRetries)
+                    {
+                        Console.WriteLine("Достигнуто максимальное количество повторов. Вставка не удалась.");
+                        throw;
+                    }
+
+                    // Логирование дополнительных данных о проблеме, если необходимо
+                    Console.WriteLine("Дополнительная информация для отладки:");
+                    Console.WriteLine(ex.StackTrace);
+                }
+            }
+        }
+
+        public static DateTime GetRandomDateTime(DateTime startDate, DateTime endDate)
+        {
+            Random random = new Random();
+            int range = (endDate - startDate).Days;
+            int randomDays = random.Next(range);
+            int randomMilliseconds = random.Next(0, 86400000); // 86400000 миллисекунд = 24 часа
+
+            DateTime randomDate = startDate.AddDays(randomDays).AddMilliseconds(randomMilliseconds);
+
+            return randomDate;
+        }
+
+        public static decimal GetRandomNonZeroFractionalAmount(Random random)
+        {
+            int integerPart = random.Next(1, 1000000);
+            double fractionalPart;
+            do
+            {
+                fractionalPart = random.NextDouble(); // Случайное число от 0.0 до 1.0
+            } while (Math.Abs(fractionalPart) < 1e-6); // Повторяем, пока дробная часть не станет ненулевой
+
+            decimal amount = (decimal)(integerPart + fractionalPart);
+            return Math.Round(amount, 4); // Округление до 4 знаков после запятой
+        }
+
+        static async Task<List<Guid>> ReadAccountIdsFromFile(string filePath)
+        {
+            var lines = await File.ReadAllLinesAsync(filePath);
+            var res =  lines.Select(line => Guid.Parse(line)).ToList();
+            return res;
+        }
 }
